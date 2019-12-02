d6f8f251a2ddf7dc67109b0e42ff8cd27a28f0e5
Дмитрий Десятников <sickmeppo@gmail.com>
2019-12-02 15:13:04 +0400

    init project to git

---
+++ src/components/AddList/index.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import List from "../List";
+
+import './AddListButtom.scss'
+import Badge from "../Badge";
+
+const AddList = ({colors}) => {
+    const [visiblePopup, setVisiblePopup] = useState(false);
+    const [selectColor, setSelectColor] = useState(null);
+    return (
+        <div className="add-list">
+            <List onClick={() => setVisiblePopup(true)} items={[
+                {
+                    className: 'list__add-button',
+                    icon:
+                        <svg width="12" height="12" viewBox="0 0 12 12" fill="none"
+                             xmlns="http://www.w3.org/2000/svg" className="list__icon-plus">
+                            <path d="M6 1V11" stroke="#868686" strokeWidth="1.5" strokeLinecap="round"
+                                  strokeLinejoin="round"/>
+                            <path d="M1 6H11" stroke="#868686" strokeWidth="1.5" strokeLinecap="round"
+                                  strokeLinejoin="round"/>
+                        </svg>,
+                    name: 'Добавить список'
+                }
+
+            ]}/>
+            {visiblePopup &&
+            <div className="add-list__popup">
+                <input className="field" type="text" placeholder="Название папки"/>
+                <div className="add-list__popup-colors">
+                    {colors.map(color => (
+                        <Badge
+                            onClick={() => setSelectColor(color.id)}
+                            key={color.id}
+                            color={color.name}
+                            className={selectColor === color.id && 'active'}
+                        />
+                    ))}
+                </div>
+                <button onClick={() => setVisiblePopup(false)} className="button">Добавить</button>
+            </div>
+            }
+        </div>
+
+    );
+};
+
+export default AddList;
